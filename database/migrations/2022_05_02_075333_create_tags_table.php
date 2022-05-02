4fb385e64196fdd601d037012e1142e8c886b9d3
Arman Arif <arman3472rmn@gmail.com>
2022-05-03 02:02:12 +0600

    initialized tags

--- database/migrations/2022_05_02_075333_create_tags_table.php
+++ database/migrations/2022_05_02_075333_create_tags_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
         });
+
+        Tag::create(['name' => 'has_air_conditioning']);
+        Tag::create(['name' => 'has_private_bathroom']);
+        Tag::create(['name' => 'has_coffee_machine']);
     }
 
     /**
